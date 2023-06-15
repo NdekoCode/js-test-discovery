0eec5de250fbc0ba3fadb68bbeeb5e73df674a3e
NdekoCode <arickbulakali@gmail.com>
2023-06-15 10:30:32 +0200

    test: add scenario testing and throw failed operation with test

--- __test__/demo.spec.js
+++ __test__/demo.spec.js
@@ -3,7 +3,20 @@ test("Test addition", () => {
   const valueAdd = add(2, 5);
   expect(valueAdd).toBe(7);
 });
-test("Test multiplication", () => {
-  const valueMulti = multi(2, 1);
-  expect(valueMulti).toBe(4);
+describe("Test multiplication scenario", () => {
+  test("Multiplication not null", () => {
+    const valueMulti = multi(2, 2);
+    expect(valueMulti).toBe(4);
+  });
+  test("Multiplication is Null", () => {
+    const valueMulti = multi(0, 5);
+    expect(valueMulti).toBe(0);
+  });
+  test("Multiplication is neutral", () => {
+    const valueMulti = multi(2, 1);
+    expect(valueMulti).toBe(2);
+  });
+  test("Multiplication Error", () => {
+    expect(() => multi(4, 0)).toThrow("Not divisible");
+  });
 });
--- libs/functions.js
+++ libs/functions.js
@@ -3,6 +3,15 @@ module.exports = {
     return a + b;
   },
   multi(a, b) {
+    if (
+      b === 0 ||
+      a === undefined ||
+      b === undefined ||
+      typeof a !== "number" ||
+      typeof b !== "number"
+    ) {
+      throw new Error("Not divisible");
+    }
     return a * b;
   },
 };
