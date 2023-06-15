bf3cd3a53feff3b2ac9f84a456ac3c1aa2d8c2ad
NdekoCode <arickbulakali@gmail.com>
2023-06-15 10:01:22 +0200

    testing using commonJS with expect

--- __test__/demo.spec.js
+++ __test__/demo.spec.js
@@ -1,3 +1,9 @@
-test("Demo", () => {
-  console.log("First Test");
+const { add, multi } = require("../libs/functions");
+test("Test addition", () => {
+  const valueAdd = add(2, 5);
+  expect(valueAdd).toBe(7);
+});
+test("Test multiplication", () => {
+  const valueMulti = multi(2, 1);
+  expect(valueMulti).toBe(4);
 });
