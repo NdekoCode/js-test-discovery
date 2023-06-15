9ba7b1a5acc1bd7b6b3f826b4328ff04609da7b3
NdekoCode <arickbulakali@gmail.com>
2023-06-15 10:43:52 +0200

    replace test by it in testing scenario

--- __test__/demo.spec.js
+++ __test__/demo.spec.js
@@ -3,7 +3,9 @@ test("Test addition", () => {
   const valueAdd = add(2, 5);
   expect(valueAdd).toBe(7);
 });
-describe("Test multiplication scenario", () => {
+
+// Scenario TEST
+/* describe("Test multiplication scenario", () => {
   test("Multiplication not null", () => {
     const valueMulti = multi(2, 2);
     expect(valueMulti).toBe(4);
@@ -16,7 +18,28 @@ describe("Test multiplication scenario", () => {
     const valueMulti = multi(2, 1);
     expect(valueMulti).toBe(2);
   });
+  //   test IF a method throw a exception
   test("Multiplication Error", () => {
+    expect(() => multi(4, 0)).toThrow("Not divisible");
+  });
+});
+ */
+
+describe("Test multiplication scenario", () => {
+  it("Should make a Multiplication not null", () => {
+    const valueMulti = multi(2, 2);
+    expect(valueMulti).toBe(4);
+  });
+  it("Should make a null multiplication", () => {
+    const valueMulti = multi(0, 5);
+    expect(valueMulti).toBe(0);
+  });
+  it("Should do a neutral Multiplication", () => {
+    const valueMulti = multi(2, 1);
+    expect(valueMulti).toBe(2);
+  });
+  //   test IF a method throw a exception
+  it("Shout trow a error in Multiplication", () => {
     expect(() => multi(4, 0)).toThrow("Not divisible");
   });
 });
